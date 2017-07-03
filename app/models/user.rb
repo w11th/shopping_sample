9caf5adf852901a8b3a5dc24d4da01fcfd75ad81
Wenhui Wang <w.eleventh@gmail.com>
2017-07-03 15:15:45 +0800

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates_presence_of :email, message: '邮箱不能为空'
+  validates :email, uniqueness: true
+
+  validates_presence_of :password, message: '密码不能为空', if: :need_validate_password
+  validates_presence_of :password_confirmation, message: '密码确认不能为空', if: :need_validate_password
+  validates_confirmation_of :password, message: '密码不一致', if: :need_validate_password
+  validates_length_of :password, minimum: 6, message: '密码最短为 6 位', if: :need_validate_password
+
+  private
+
+  def need_validate_password
+    new_record? || !(password.nil? && password_confirmation.nil?)
+  end
 end
