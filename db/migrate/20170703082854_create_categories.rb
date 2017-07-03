5d90493bc1ea16943973495e5450bcc589ba292f
Wenhui Wang <w.eleventh@gmail.com>
2017-07-03 16:53:05 +0800

    Model: category product

---
+++ db/migrate/20170703082854_create_categories.rb
@@ -0,0 +1,13 @@
+class CreateCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :categories do |t|
+      t.string :title
+      t.integer :weight, default: 0
+      t.integer :products_counter, default: 0
+
+      t.timestamps
+    end
+
+    add_index :categories, [:title]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630081605) do
+ActiveRecord::Schema.define(version: 20170703082913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.integer "weight", default: 0
+    t.integer "products_counter", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_categories_on_title"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "category_id"
+    t.string "title"
+    t.string "status", default: "off"
+    t.integer "amount", default: 0
+    t.string "uuid"
+    t.decimal "msrp", precision: 10, scale: 2
+    t.decimal "price", precision: 10, scale: 2
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["status", "category_id"], name: "index_products_on_status_and_category_id"
+    t.index ["title"], name: "index_products_on_title"
+    t.index ["uuid"], name: "index_products_on_uuid"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
